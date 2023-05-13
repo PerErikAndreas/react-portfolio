a8704df77692f3ef08b0b9ac00104d6bcc3d53cb
Andreas Axelsson <pererikandreas@gmail.com>
2023-05-13 21:39:23 +0200

    changed from woff to otf

--- src/App.js
+++ src/App.js
@@ -6,30 +6,24 @@ import { Sec3FeaturedProjects } from 'Components/Sections/Sec3FeaturedProjects';
 import { Sec4MyWords } from 'Components/Sections/Sec4MyWords';
 import { Sec5Skills } from 'Components/Sections/Sec5Skills';
 import { Sec6TimeToTalk } from 'Components/Sections/Sec6TimeToTalk';
-import SatoshiBlackWoff from './Fonts/Satoshi/Fonts/WEB/fonts/Satoshi-Black.woff';
-import SatoshiBlackWoff2 from './Fonts/Satoshi/Fonts/WEB/fonts/Satoshi-Black.woff2';
-import SatoshiBoldWoff from './Fonts/Satoshi/Fonts/WEB/fonts/Satoshi-Bold.woff';
-import SatoshiBoldWoff2 from './Fonts/Satoshi/Fonts/WEB/fonts/Satoshi-Bold.woff2';
-import SatoshiMediumWoff from './Fonts/Satoshi/Fonts/WEB/fonts/Satoshi-Medium.woff';
-import SatoshiMediumWoff2 from './Fonts/Satoshi/Fonts/WEB/fonts/Satoshi-Medium.woff2';
+import SatoshiBlackOTF from './Fonts/Satoshi/Fonts/OTF/Satoshi-Black.otf';
+import SatoshiBoldOTF from './Fonts/Satoshi/Fonts/OTF/Satoshi-Bold.otf';
+import SatoshiMediumOTF from './Fonts/Satoshi/Fonts/OTF/Satoshi-Medium.otf';
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'Satoshi';
-    src: url(${SatoshiBlackWoff}) format('woff2'),
-         url(${SatoshiBlackWoff2}) format('woff');
+    src: url(${SatoshiBlackOTF}) format('opentype'),
   }
 
   @font-face {
     font-family: 'Satoshi';
-    src: url(${SatoshiBoldWoff}) format('woff2'),
-         url(${SatoshiBoldWoff2}) format('woff');
+    src: url(${SatoshiBoldOTF}) format('opentype'),
   }
 
   @font-face {
     font-family: 'Satoshi';
-    src: url(${SatoshiMediumWoff}) format('woff2'),
-         url(${SatoshiMediumWoff2}) format('woff');
+    src: url(${SatoshiMediumOTF}) format('opentype'),
   }
 `;
 
