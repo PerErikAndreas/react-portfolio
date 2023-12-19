40eeb653742023916e2fc29403520ebd27d381b2
Andreas Axelsson <pererikandreas@gmail.com>
2023-12-19 12:37:58 +0100

    started implementing chatbot

---
+++ src/Components/Chatbot/ChatInput.jsx
@@ -0,0 +1,51 @@
+// // //////////////////////////////////////////////////////////////////////// //
+// // //////////////////////////// IMPORTS /////////////////////////////////// //
+// // //////////////////////////////////////////////////////////////////////// //
+
+// import { useState } from "react";
+// import sendButtonImg from '../assets/sent.png'
+// import clean from '../assets/clean.png'
+
+// // //////////////////////////////////////////////////////////////////////// //
+// // /////////////////////////// CHAT INPUT ///////////////////////////////// //
+// // //////////////////////////////////////////////////////////////////////// //
+
+// export const ChatInput = ({ responseGenerate }) => {
+//   const [message, setMessage] = useState("");
+
+//   const onFormSubmit = (e) => {
+//     e.preventDefault();
+//     responseGenerate(message, setMessage);
+//   };
+
+//   const handleKeyDown = (e) => {
+//     if (e.key === "Enter" && !e.shiftKey) {
+//       e.preventDefault();
+//       onFormSubmit(e);
+//     }
+//   };
+
+// // //////////////////////////////////////////////////////////////////////// //
+// // /////////////////////////////// RETURN ///////////////////////////////// //
+// // //////////////////////////////////////////////////////////////////////// //
+
+//   return (
+//     <div className="inputplusbutton">
+//     <textarea
+//       rows="2"
+//       type="text"
+//       className="input"
+//       placeholder="Whats on your mind?"
+//       value={message}
+//       onChange={(e) => setMessage(e.target.value)}
+//       onKeyDown={handleKeyDown}
+//     />
+//     <br />
+//     <form onSubmit={onFormSubmit}>
+//       <button type="submit" className="sendButton">
+//         <img src={sendButtonImg} alt="send button" className="icons" />
+//       </button>
+//     </form>
+//   </div>
+// );
+// };
